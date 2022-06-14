e26de390ce9c4f12edd89f025961173a0996e0f3
stephen rogan <srogan26@gmail.com>
2022-06-13 21:37:57 -0500

    created Map objects for conditional outcomes based on path choice  (enemy and next possible path choices). Also, added functions for conditional prompts and condensed the combat menu using a choices Array to conditionally add the actions to as they apply

--- RPG.js
+++ RPG.js
@@ -244,16 +244,39 @@ const blackBear = new Character('Black Bear', 'Beast', 150, 40, 15, 20);//Mounta
 const clayGolem = new Character('Clay Golem', 'Elemental', 150, 40, 15, 20);//Cave Path
 const undine = new Character('Undine', 'Elemental', 150, 40, 15, 20);//Seaside Path
 const salamander = new Character('Salamander', 'Elemental', 150, 40, 15, 20);//Volcanic Path
-//Created Cursed class Enemies
+//Created Undead class Enemies
 const zombie = new Character('Zombie', 'Undead', 150, 40, 15, 20);//Forest Path
 const mummy = new Character('Mummy', 'Undead', 150, 40, 15, 20);//Tombs Path
 const vampire = new Character('Vampire', 'Undead', 150, 40, 15, 20);//Secluded Mansion Path
+//Generate a Path:Enemy Map object for reference when determining encounters
+const pathEnemyMap = new Map();
+//First Path:Enemy Set
+pathEnemyMap.set('Sewer', bigRat);
+pathEnemyMap.set('Cave', clayGolem);
+pathEnemyMap.set('Forest', zombie);
+//Second Path:Enemy Set
+pathEnemyMap.set('Ghost Town', hungryWolf);
+pathEnemyMap.set('Seaside', undine);
+pathEnemyMap.set('Tombs', mummy);
+//Third Path:Enemy Set
+pathEnemyMap.set('Mountainous', blackBear);
+pathEnemyMap.set('Volcanic', salamander);
+pathEnemyMap.set('Secluded Mansion', vampire);
+//Generate the set of conditional path choices Map object for the second path select
+const choiceMap = new Map();
+choiceMap.set('Sewer', ['Seaside', 'Tombs']);
+choiceMap.set('Cave', ['Ghost Town', 'Tombs']);
+choiceMap.set('Forest', ['Ghost Town', 'Seaside']);
+choiceMap.set('Ghost Town', ['Volcanic', 'Secluded Mansion']);
+choiceMap.set('Seaside', ['Mountainous', 'Secluded Mansion']);
+choiceMap.set('Tombs', ['Mountainous', 'Volcanic']);
 //Pre-generate an Array to store the characters in our party
 const partyMembers = new Array();
 //Function to Read out your current party members, Takes in your party members Array
 const partyReadOut = (party) => {
+  console.log('Your Party Members Are:')
   party.forEach(member => {
-    console.log(`Your party: ${member.name} the ${member.role}`);
+    console.log(`${member.name} the ${member.role}`);
   });
 }
 //main function to run game
@@ -275,22 +298,22 @@ const main = async () => {
   const firstPath = await firstChoice();
   const pathOne = firstPath.route;
   const firstEnemy = generateEncounter(pathOne);
-  const firstResult = await partyBattle(partyMembers, firstEnemy);
-  if (firstResult.party[0].currentHealth <= 0) {
-    console.log('You Died....');
-    return;
-  } else console.log(`The enemy has ${firstResult.enemy.currentHealth} health remaining! You\'ve subdued the ${firstResult.enemy.role}!`);
-  partyMembers.push(firstEnemy);
-  partyReadOut(partyMembers);
-  //Currently testing party battle functionality
-  console.log('Fight the testing dummy!!!');
-  const secondResult = await partyBattle(partyMembers, testDummy);
-  if (secondResult.party[0].currentHealth <= 0) {
-    console.log('You Died....');
-    return;
-  } else console.log(`The enemy has ${secondResult.enemy.currentHealth} health remaining! You\'ve subdued the ${secondResult.enemy.role}!`);
-  partyMembers.push(testDummy);
-  partyReadOut(partyMembers);
+  //Handles initiating combat and determing victory, ends programs if you lose
+  let isDead = await initiateCombat(partyMembers, firstEnemy);
+  if (isDead) return;//Ends game if Dead
+  recruitMember(partyMembers, firstEnemy);//Adds defeated enemy to party
+  const secondPath = await conditionalPath(pathOne);
+  const pathTwo = secondPath.route
+  const secondEnemy = generateEncounter(pathTwo);
+  isDead = await initiateCombat(partyMembers, secondEnemy);
+  if (isDead) return;//Ends game if Dead
+  recruitMember(partyMembers, secondEnemy);//Adds defeated enemy to party
+  const thirdPath = await conditionalPath(pathTwo);
+  const pathThree = thirdPath.route
+  const thirdEnemy = generateEncounter(pathThree);
+  isDead = await initiateCombat(partyMembers, thirdEnemy);
+  if (isDead) return;//Ends game if Dead
+  recruitMember(partyMembers, thirdEnemy);//Adds defeated enemy to party
 }
 //Initial grreting function on game start
 const greeting = () => {
@@ -367,75 +390,93 @@ const firstChoice = async () => {
     ]);
   return firstPath;
 }
-//function to determine first encounter
-const generateEncounter = (path) => {
-  let firstFight
-  switch (path) {
+//Context text depending on previous path choice
+const pathSelectText = (prevPath) => {
+  switch (prevPath) {
     case 'Sewer':
-      console.log('You Encounter a Big Rat!');
-      firstFight = bigRat;
+      console.log('Left is the sea. Right is an open tomb.')
       break;
     case 'Cave':
-      console.log('You encounter a Clay Golem!');
-      firstFight = clayGolem;
+      console.log('Left is a ghost town. Right is an open tomb.')
       break;
     case 'Forest':
-      console.log('You encounter a Zombie!')
-      firstFight = zombie;
+      console.log('Left is the sea. Right is a ghost town.')
+      break;
+    case 'Ghost Town':
+      console.log('Left is a Volcanic area. Right is a Secluded Mansion.')
+      break;
+    case 'Seaside':
+      console.log('Left is a Mountainous area. Right is a Secluded Mansion.')
+      break;
+    case 'Tombs':
+      console.log('Left is a Mountainous area. Right is a Volcanic area.')
       break;
   }
-  return firstFight;
 }
-
+//Conditional Prompt based on First path selected
+const conditionalPath = async (prevPath) => {
+  pathSelectText(prevPath);
+  const pathChoices = choiceMap.get(prevPath)
+  const nextPath = await inquirer
+    .prompt([
+      {
+        name: 'route',
+        type: 'list',
+        message: 'Which path will you follow?',
+        choices: pathChoices
+      }
+    ]);
+  return nextPath;
+}
+//function to determine first encounter
+const generateEncounter = (path) => {
+  let pathEnemy = pathEnemyMap.get(path);
+  return pathEnemy;
+}
+//function to initiate battle and check for Victory or defeat
+const initiateCombat = async (partyMembers, firstEnemy) => {
+  const battleResult = await partyBattle(partyMembers, firstEnemy);
+  if (battleResult.party[0].currentHealth <= 0) {
+    console.log('You Died....');
+    let isDead = true
+    return isDead;
+  } else console.log(`The enemy has ${battleResult.enemy.currentHealth} health remaining! You\'ve subdued the ${battleResult.enemy.role}!`);
+}
+//Adds Member to party when victorious in battle, Arg 1 is party Array Arg 2 is enemy character
+const recruitMember = (partyMembers, enemy) => {
+  partyMembers.push(enemy);//Adds the enemy to your party if victorious
+  partyReadOut(partyMembers);
+}
 /**The function for the actual battle prompts, 
  * takes in a player object argument to check status of character and determine what actions are relevent to their status
 */
 const inCombatMenu = async (fighter) => {
   let turn
+  const statusBasedChoices = new Array();
+  statusBasedChoices.push('Attack');
   console.log(
     `${fighter.name} has:
   HP: ${fighter.currentHealth}/${fighter.maxHealth} 
   SP: ${fighter.currentSP}/${fighter.maxSP}`);
-  //Check for case where resources are full and remove recover action since it will do nothing
-  if (fighter.currentHealth === fighter.maxHealth && fighter.currentSP === fighter.maxSP) {
-    turn = await inquirer
-      .prompt([
-        {
-          name: 'action',
-          type: 'list',
-          message: `What will ${fighter.name} do?`,
-          choices: ['Attack', 'Skill']
-        }
-      ]);
-    return turn;
-  } 
-  //Remove Skill action in case where the player doesn't have enough SP
-  else if(fighter.currentSP < fighter.skillCost){
-    console.log(`${fighter.name} doesn't have enough SP to use their skill!`);
-    turn = await inquirer
-      .prompt([
-        {
-          name: 'action',
-          type: 'list',
-          message: `What will ${fighter.name} do?`,
-          choices: ['Attack', 'Rest and Recover']
-        }
-      ]);
-    return turn;
-  }
-  //All other scenarios will have all three action options available
-  else {
-    turn = await inquirer
-      .prompt([
-        {
-          name: 'action',
-          type: 'list',
-          message: `What will ${fighter.name} do?`,
-          choices: ['Attack', 'Skill', 'Rest and Recover']
-        }
-      ]);
-    return turn;
-  }
+  //Add Skill action to possible choices in case where the player has enough SP to use skill
+  if (fighter.currentSP >= fighter.skillCost) {
+    console.log(`${fighter.name} has enough SP to use their skill!!!`);
+    statusBasedChoices.push('Skill');
+  };
+  //Check for case where either resource isn't full and add recover action to possible choices
+  if (fighter.currentHealth !== fighter.maxHealth || fighter.currentSP !== fighter.maxSP) {
+    statusBasedChoices.push('Rest and Recover');
+  };
+  turn = await inquirer
+    .prompt([
+      {
+        name: 'action',
+        type: 'list',
+        message: `What will ${fighter.name} do?`,
+        choices: statusBasedChoices
+      }
+    ]);
+  return turn;
 }
 /**Player Action Selection, handles the battle menu selection and execution of the selected action(s)
  * Argument 1 is party Array of Character Objects,
@@ -452,19 +493,21 @@ const playerAction = async (combatParty, combatEnemy) => {
   };
   //Uses list of actions that were chosen in combat menu
   combatParty.forEach(member => {
-    switch (member.action) {
-      case 'Rest and Recover':
-        member.recover();
-        break;
-      case 'Attack':
-        member.attack(combatEnemy);
-        break;
-      case 'Skill':
-        member.roleSkill(combatEnemy, combatParty);
-        break;
-      case 'incap':
-        console.log(`${member.name} has ${member.currentHealth} health left and could not act...`)
-        break;
+    if (combatEnemy.currentHealth > 0) {
+      switch (member.action) {
+        case 'Rest and Recover':
+          member.recover();
+          break;
+        case 'Attack':
+          member.attack(combatEnemy);
+          break;
+        case 'Skill':
+          member.roleSkill(combatEnemy, combatParty);
+          break;
+        case 'incap':
+          console.log(`${member.name} has ${member.currentHealth} health left and could not act...`)
+          break;
+      }
     }
   });
 }
