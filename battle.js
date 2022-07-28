056f0e1ed549fdce40ef47dd53db0bc938b56b0a
stephen rogan <srogan26@gmail.com>
2022-07-27 19:32:00 -0500

    Added menu js file and started making additional inquirer menues

--- battle.js
+++ battle.js
@@ -8,57 +8,8 @@ const {
   waitFor,
   gameConsole
 } = require('./util.js')
-//Skill Sub-Menu for specific skill selection
-const skillSubMenu = async (fighter) => {
-  const usableSkills = fighter.learnedSkills.filter(skill => skill.skillCost <= fighter.currentSP);
-  const skillChoices = usableSkills.map(skill => skill.name);
-  skillChoices.push('Cancel');
-  let turn = await inquirer
-    .prompt([
-      {
-        name: 'action',
-        type: 'list',
-        message: `Which skill will ${fighter.name} use?`,
-        choices: skillChoices
-      }
-    ]);
-  //Handling for the Cancel Option to "go back" (not really) to Main combat menu
-  if (turn.action === 'Cancel') turn = await combatMenu(fighter);
-  return turn;
-}
-/**The function for the actual battle prompts, 
- * takes in a player object argument to check status of character and determine what actions are relevent to their status
-*/
-const combatMenu = async (fighter) => {
-  const statusBasedChoices = new Array();
-  statusBasedChoices.push('Attack');
-  console.log(
-    `${fighter.name} has:
-    HP: ${fighter.currentHealth}/${fighter.maxHealth} 
-    SP: ${fighter.currentSP}/${fighter.maxSP}`);
-  //Add Skill action to possible choices in case where the player has enough SP to use skill
-  const skillCosts = fighter.learnedSkills.map(skill => skill.skillCost);
-  if (fighter.currentSP >= Math.min(...skillCosts)) {
-    console.log(`${fighter.name} has enough SP to use a skill!!!`);
-    statusBasedChoices.push('Skill');
-  };
-  //Check for case where either resource isn't full and add recover action to possible choices
-  if (fighter.currentHealth !== fighter.maxHealth || fighter.currentSP !== fighter.maxSP) {
-    statusBasedChoices.push('Rest and Recover');
-  };
-  let turn = await inquirer
-    .prompt([
-      {
-        name: 'action',
-        type: 'list',
-        message: `What will ${fighter.name} do?`,
-        choices: statusBasedChoices
-      }
-    ]);
-  //Sub Menu Handling
-  if (turn.action === 'Skill') turn = await skillSubMenu(fighter);
-  return turn;
-}
+//import menues
+const {combatMenu} = require('./menu.js');
 //Function to handle for buff and debuff duration
 const handleStatModifierDuration = async (combatParty, combatEnemy, currentTurn) => {
   //checking player buff and debuff duration
@@ -299,8 +250,6 @@ const partyBattle = async (party, enemy) => {
 };
 
 module.exports = {
-  skillSubMenu,
-  combatMenu,
   playerAction,
   enemyActionRoll,
   partyBattle
